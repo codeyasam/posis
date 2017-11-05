27399256496b098f11d91479b2c3a0578d249a90
Yasa Emmanuel <codeyasam@gmail.com>
2017-11-05 21:52:26 +0800

    adds getters and setters to Role Entity

--- src/main/java/com/codeyasam/posis/domain/security/Role.java
+++ src/main/java/com/codeyasam/posis/domain/security/Role.java
@@ -16,6 +16,23 @@ public class Role {
 	
 	@Column(nullable=false, unique=true)
 	private String role;
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 	
 	
 }
