fcfc133df143e7d1963dbf279f26883dda4ffc15
Yasa Emmanuel <codeyasam@gmail.com>
2017-11-14 10:37:16 +0800

    add UserAlreadyExist and UserNotFound Exceptions

--- src/main/java/com/codeyasam/posis/controller/ExceptionControllerAdvice.java
+++ src/main/java/com/codeyasam/posis/controller/ExceptionControllerAdvice.java
@@ -9,12 +9,24 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.codeyasam.posis.exception.PageNotFoundException;
+import com.codeyasam.posis.exception.UserAlreadyExistException;
+import com.codeyasam.posis.exception.UserNotFoundException;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 	
 	@ExceptionHandler(PageNotFoundException.class)
 	public void handlePageNotFoundException(PageNotFoundException exception, HttpServletResponse response) throws IOException {
+		response.sendError(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+	}
+	
+	@ExceptionHandler(UserAlreadyExistException.class)
+	public void handleUserAlreadyExistException(UserAlreadyExistException exception, HttpServletResponse response) throws IOException {
+		response.sendError(HttpStatus.CONFLICT.value(), exception.getMessage());
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public void handleUserNotFoundException(UserNotFoundException exception, HttpServletResponse response) throws IOException {
 		response.sendError(HttpStatus.NOT_FOUND.value(), exception.getMessage());
 	}
 }
