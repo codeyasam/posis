fffd78073d59498bd503223fb4c84582b89d1cb7
Yasa Emmanuel <codeyasam@gmail.com>
2017-11-15 22:59:52 +0800

    adds retrieve by full name in EndUser specification

--- src/main/java/com/codeyasam/posis/domain/specification/EndUserSpecification.java
+++ src/main/java/com/codeyasam/posis/domain/specification/EndUserSpecification.java
@@ -1,5 +1,8 @@
 package com.codeyasam.posis.domain.specification;
 
+
+import javax.persistence.criteria.Expression;
+
 import org.springframework.data.jpa.domain.Specification;
 
 import com.codeyasam.posis.domain.security.EndUser;
@@ -7,11 +10,24 @@ import com.codeyasam.posis.domain.security.EndUser;
 public class EndUserSpecification {
 	
 	public static Specification<EndUser> textInAllColumns(String text) {
+		text = text.trim();
+		if (text.isEmpty()) {
+			text = "%" +  text + "%";
+		}
+		
 		final String finalText = text;
-		return (root, query, builder) -> builder.or(
-				builder.like(root.get("lastName"), finalText),
+		return (root, query, builder) -> {
+			Expression<String> fullNameExpFirstNameFirst = builder.concat(root.get("firstName"), " ");
+			fullNameExpFirstNameFirst = builder.concat(fullNameExpFirstNameFirst, root.get("lastName"));
+			Expression<String> fullNameExpLastNameFirst  = builder.concat(root.get("lastName"), " ");
+			fullNameExpLastNameFirst = builder.concat(fullNameExpLastNameFirst, root.get("firstName"));
+			
+			return builder.or(
+				builder.like(fullNameExpFirstNameFirst, finalText),
+				builder.like(fullNameExpLastNameFirst, finalText),
 				builder.like(root.get("firstName"), finalText),
+				builder.like(root.get("lastName"), finalText),
 				builder.like(root.get("username"), finalText)
-		);
+		);};
 	}
 }
