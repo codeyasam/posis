bc24b8f0942aef954bcb76abea19b824ece09303
Yasa Emmanuel <codeyasam@gmail.com>
2017-11-15 23:07:32 +0800

    removes password from EndUserDTO

--- src/main/java/com/codeyasam/posis/dto/EndUserDTO.java
+++ src/main/java/com/codeyasam/posis/dto/EndUserDTO.java
@@ -10,7 +10,6 @@ public class EndUserDTO {
 	private String firstName;
 	private String lastName;
 	private String username;
-	private String password;
 	
 	private Set<Role> roles;
 
@@ -44,14 +43,6 @@ public class EndUserDTO {
 
 	public void setUsername(String username) {
 		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public Set<Role> getRoles() {
