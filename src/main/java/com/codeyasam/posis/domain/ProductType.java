d545438b1b710a2e88ff59c0687bf3d6fdf07340
Yasa Emmanuel <codeyasam@gmail.com>
2017-12-06 21:25:40 +0800

    added postgresql dependency and result set mapping to product type

--- src/main/java/com/codeyasam/posis/domain/ProductType.java
+++ src/main/java/com/codeyasam/posis/domain/ProductType.java
@@ -1,16 +1,32 @@
 package com.codeyasam.posis.domain;
 
+import javax.persistence.ColumnResult;
+import javax.persistence.ConstructorResult;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedStoredProcedureQueries;
+import javax.persistence.NamedStoredProcedureQuery;
+import javax.persistence.SqlResultSetMapping;
 import javax.persistence.Table;
+
+import com.codeyasam.posis.projection.ProductTypeProjection;
 
 @Entity
 @Table(name="product_type")
+@SqlResultSetMapping(name = "ProductTypeMapping", classes = {
+		@ConstructorResult(targetClass = ProductTypeProjection.class, 
+				columns = {
+						@ColumnResult(name="type_id", type=Long.class),
+						@ColumnResult(name="type_name", type=String.class)
+				})
+})
 public class ProductType {
 	
 	@Id
-	@GeneratedValue
+//	@Column(columnDefinition="serial")	
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	private String name;
 
