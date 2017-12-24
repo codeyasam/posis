a35dbd8af73f2a0992df1b783470341d1b147db9
Yasa Emmanuel <codeyasam@gmail.com>
2017-12-24 17:46:16 +0800

    adds a auditoraware fields and orders roles

--- src/main/java/com/codeyasam/posis/domain/security/EndUser.java
+++ src/main/java/com/codeyasam/posis/domain/security/EndUser.java
@@ -1,17 +1,27 @@
 package com.codeyasam.posis.domain.security;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name="end_user")
 public class EndUser {
 	
@@ -31,8 +41,23 @@ public class EndUser {
 	private String status;
 	
 	@ManyToMany(fetch=FetchType.EAGER)
+	@OrderBy("id asc")
 	private Set<Role> roles;
 
+	@CreatedDate
+	@Column(updatable=false)
+	private LocalDateTime createdDate;
+	
+	@LastModifiedDate
+	@Column(insertable=false)
+	private LocalDateTime lastModifiedDate;
+
+	@CreatedBy
+	private String createdBy;
+	
+	@LastModifiedBy
+	private String lastModifiedBy;
+	
 	public long getId() {
 		return id;
 	}
@@ -88,5 +113,37 @@ public class EndUser {
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
 	}
+	
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	public void setLastModifiedBy(String lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}		
 	
 }
