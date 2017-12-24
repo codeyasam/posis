ad275474556beb2bc8662fbeb90841be20aa7f71
Yasa Emmanuel <codeyasam@gmail.com>
2017-12-24 17:42:51 +0800

    changes auditoraware from user to string(username)

--- src/main/java/com/codeyasam/posis/config/PersistenceConfig.java
+++ src/main/java/com/codeyasam/posis/config/PersistenceConfig.java
@@ -4,14 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef="auditorProvider")
 public class PersistenceConfig {
 	
 	@Bean
-	public AuditorAware<User> auditorProvider() {
+	public AuditorAware<String> auditorProvider() {
 		return new AuditorAwareImpl();
 	}
 }
