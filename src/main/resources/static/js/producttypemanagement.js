d7497e8cf75aead324561e923b6547cff496eb55
Yasa Emmanuel <codeyasam@gmail.com>
2017-12-27 13:52:40 +0800

    implements searchInSpecified columns

--- src/main/resources/static/js/producttypemanagement.js
+++ src/main/resources/static/js/producttypemanagement.js
@@ -28,11 +28,11 @@ var productTypeBufferedStore = new Posis.store.ProductTypeStore();
 productTypeBufferedStore.getProxy().url = '/posis/endproduct/type/?size=5&sort=id';		
 productTypeBufferedStore.load();
 
-Ext.define('Posis.view.ProductTypeList', {
+Ext.define('Posis.view.ProductCategoriesList', {
 	extend: 'Ext.grid.Panel',
 	title: 'Product Categories',
-	alias: 'widget.producttypeslist',
-	id: 'producttypeslist',
+	alias: 'widget.ProductCategoriesList',
+	id: 'ProductCategoriesList',
 	store: 'ProductTypeStore',
 	height: 250,
 	scrollable: true,
@@ -85,13 +85,43 @@ Ext.define('Posis.view.ProductTypeList', {
         		productTypeBufferedStore.getProxy().url = '/posis/endproduct/type/?size=5&sort=id'; 
         			+ '&page=' + encodeURIComponent(page) + '&search=' + encodeURIComponent(searchText);
         		productTypeBufferedStore.load();
-        		var producttypeslist = Ext.getCmp('producttypeslist');
+        		var ProductCategoriesList = Ext.getCmp('ProductCategoriesList');
         		var mainContainer = Ext.getCmp('mainContainer');
         		setTimeout(function() {
-        			producttypeslist.reconfigure(productTypeBufferedStore);
+        			ProductCategoriesList.reconfigure(productTypeBufferedStore);
         			mainContainer.updateLayout();
         		}, 500);
         	}
         }
     }	
+});
+
+Ext.define('Posis.controller.ProductTypeController', {
+	extend: 'Ext.app.Controller',
+	views: ['ProductCategoriesList'],
+	refs: [{
+		ref: 'ProductCategoriesList',
+		xtype: 'ProductCategoriesList',
+		selector: 'ProductCategoriesList'
+	}],
+	init: function() {
+		this.control({
+			'ProductCategoriesList > toolbar > textfield[action=search]': {
+				keyup: this.searchProductType
+			}
+		});
+	},
+	searchProductType: function() {
+		var searchText = Ext.getCmp('productTypeSearchField').getValue();
+		var productTypeStore = new Posis.store.ProductTypeStore();
+		productTypeStore.getProxy().url = '/posis/endproduct/type/?search=' + encodeURIComponent(searchText) + '&?size=5&sort=id';
+		var pagingtoolbar = Ext.getCmp('productTypePagingToolbar');
+		productTypeStore.load();
+		var ProductCategoriesList = this.getProductCategoriesList();
+		setTimeout(function() {
+			ProductCategoriesList.reconfigure(productTypeStore);
+			productTypeBufferedStore.totalCount = productTypeStore.getTotalCount();
+			pagingtoolbar.onLoad();
+		}, 500);
+	}
 });
