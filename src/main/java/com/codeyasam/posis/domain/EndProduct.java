04e71883e5a21463ae1f69c8ef0ce3043062057f
Yasa Emmanuel <codeyasam@gmail.com>
2018-01-10 21:13:45 +0800

    [Domain - EndProduct] adds EntityListener annotation

--- src/main/java/com/codeyasam/posis/domain/EndProduct.java
+++ src/main/java/com/codeyasam/posis/domain/EndProduct.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,8 +18,10 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name="end_product")
 public class EndProduct {
 	
